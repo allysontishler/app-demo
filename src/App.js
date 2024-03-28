f796e1cc5fcae20c85ded2e8d0640ac07ff06e86
Allyson Tishler <130631697+allysontishler@users.noreply.github.com>
2024-03-28 13:32:34 -0400

    ...change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Ally and MSU!
+          Learn React with Ally and MSU...
         </a>
       </header>
     </div>
