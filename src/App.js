015fe798ae3b1d527857221820cf51cc53cd89d7
Allyson Tishler <130631697+allysontishler@users.noreply.github.com>
2024-03-28 13:57:00 -0400

    const-data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const book = {
+  title: 'A Farwell to Arms',
+  author: 'Earnest Hemingway',
+  published: '1929',
+  image: 'https://upload.wikimedia.org/wikipedia/en/4/48/Hemingway_farwell.png',
+  width: '264',
+  height: '378'
+};
+
 function MagicButton() {
   return (
     <div>
@@ -14,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <MagicButton />
       </header>
     </div>
