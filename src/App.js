3316d96488368f6342ad31d46aba1d846cc7e356
Allyson Tishler <130631697+allysontishler@users.noreply.github.com>
2024-03-28 14:40:00 -0400

    lab-changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 import './App.css';
 
 const magazines = [
-  { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
-  { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
-  { id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false },
+  { id: 1, title: 'The Courage to Be Disliked', theme: 'non-fiction', isAvailable: true },
+  { id: 2, title: 'The Science Behind Karoake Machines', theme: 'non-fiction', isAvailable: true },
+  { id: 3, title: 'In Order to Live', theme: 'non-fiction', isAvailable: false },
+  { id: 4, title: 'Why I Believe I am a Vampire', theme: 'fiction', isAvailable: false },
+  { id: 5, title: 'Almond', theme: 'fiction', isAvailable: true },
 ];
 
 function ZineRack() {
@@ -12,7 +14,7 @@ function ZineRack() {
     <li
       key={zine.id}
       style={{
-        color: zine.isAvailable ? 'red' : 'green'
+        color: zine.isAvailable ? 'green' : 'red'
       }}
     >
       {zine.title}
@@ -24,37 +26,25 @@ function ZineRack() {
 }
 
 const book = {
-  title: 'A Farewell to Arms',
-  author: 'Earnest Hemingway',
-  published: '1929',
-  image: 'https://upload.wikimedia.org/wikipedia/en/4/48/Hemingway_farewell.png',
-  width: '264',
-  height: '378'
+  header: 'MSU Digital Library',
+  description: 'Find Books to Read!',
+  location: 'East Lansing, MI',
 };
 
-function Bookshelf() {
+function Library() {
   return(
     <div>
-      <h2>{book.title} ({book.published})</h2>
-      <p>{book.author}</p>
-      <img
-        className="bookCover"
-        src={book.image}
-        alt={book.title + ' cover'}
-        style={{
-          width: book.width,
-          height: book.height
-        }}
-      />  
+      <h2>{book.header} ({book.location})</h2>
+      <p>{book.description}</p>  
     </div>
   );
 }
 
-function MagicButton() {
+function GetStartedButton() {
   return (
     <div>
-      <h3>This is a magic button</h3>
-      <button>Magic</button>
+      <h3>Click here to start checking out books</h3>
+      <button>Get Started</button>
     </div>
   );
 }
@@ -63,9 +53,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Library />
         <ZineRack />
-        <Bookshelf />
-        <MagicButton />
+        <GetStartedButton />
       </header>
     </div>
   );
